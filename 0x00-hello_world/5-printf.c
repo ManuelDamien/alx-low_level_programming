409ea6717a7d30103936757ebbe3a131a099867f
ManuelDamien <manueldamienboateng@gmail.com>
2022-09-08 04:01:22 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * Description: main - program execution starting point.
- * Return : 0 when successful.
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
