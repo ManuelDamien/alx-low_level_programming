a22232214c63cd35642ca391ce593ae7f8004dff
ManuelDamien <manueldamienboateng@gmail.com>
2022-09-08 02:12:04 -0700

    Task 2 to Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ *  * Description: main - Program execution starting point.
+ *
+ *   * Return: 0 when code is successful.
+ *
+ *    */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
