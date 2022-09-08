576a090f440282b8bc7ad027ba770574cb064b58
ManuelDamien <manueldamienboateng@gmail.com>
2022-09-08 03:46:45 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - program execution starting point.
+ * Return : 0 when successful.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
