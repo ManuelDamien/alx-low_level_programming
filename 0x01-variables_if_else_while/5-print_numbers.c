97a58ad0dfc56315092029af233acf2100562a27
ManuelDamien <manueldamienboteng@gmail.com>
2022-09-09 09:52:22 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program staring point
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%x", x);
+	}
+	printf("\n");
+	return (0);
+}
