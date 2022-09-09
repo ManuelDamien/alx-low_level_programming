3c7b121ce5a84abd808fa0cfbb94e99c256d32c3
ManuelDamien <manueldamienboteng@gmail.com>
2022-09-09 10:51:13 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program execution point
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 132; alpha >= 107; alpha--)
+	{
+		putchar(alpha);
+	}
+		putchar('\n');
+		return (0);
+}
