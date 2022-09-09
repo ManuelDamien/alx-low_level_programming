c298c3478e90c0b36d766bcf72ac8e4c79daa502
ManuelDamien <manueldamienboteng@gmail.com>
2022-09-09 10:00:51 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program starting point
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar (a);
+	}
+putchar ('\n');
+return (0);
+}
