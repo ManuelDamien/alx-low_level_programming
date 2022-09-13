66f400615a84c0a02a8a769f3ac0b74bfcee784f
ManuelDamien <manueldamienboteng@gmail.com>
2022-09-13 11:36:43 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check code
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
