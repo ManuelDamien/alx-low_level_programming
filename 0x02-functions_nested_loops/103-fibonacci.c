9b9a1dd8734c00fef88c9a54a6f1c41d5161e77c
ManuelDamien <manueldamienboteng@gmail.com>
2022-09-13 13:37:46 -0700

    Task 15 update

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,17 +5,17 @@
  */
 int main(void)
 {
-	int x = 1;
-	int y = 2;
+	int a = 1;
+	int b = 2;
 	long int sum = 0;
 
 	do {
-		if (y % 2 == 0)
-			sum += y;
-		y += x;
-		x = y - x;
-	}
-	while (y <= 4000000)
-		printf("%ld\n", sum);
+		if (b % 2 == 0)
+			sum += b;
+		b += a;
+		a = b - a;
+	} while (b <= 4000000);
+
+	printf("%ld\n", sum);
 	return (0);
 }
