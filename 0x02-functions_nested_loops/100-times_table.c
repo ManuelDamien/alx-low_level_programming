4db41b15c476556ca53a68af717e51ed12f46a50
ManuelDamien <manueldamienboteng@gmail.com>
2022-09-13 13:09:58 -0700

    Task 12 update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,5 +5,32 @@
  */
 void print_times_table(int n)
 {
-return (0);
+	int i;
+	int j, k;
+
+	if (n < 0 || n > 15)
+		return;
+	for (i = 0; i <= n; ++i)
+	{
+		for (j = 0; j <= n; ++j)
+		{
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				k = i * j;
+				_putchar((k < 100) ? ' ' : ('0' + k / 100));
+				_putchar((k <  10) ? ' ' : ('0' + k / 10 % 10));
+				_putchar('0' + k % 10);
+			}
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
