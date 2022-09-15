c50567585d3dca91940dbd14efae57de9f0a5a32
ManuelDamien <manueldamienboteng@gmail.com>
2022-09-15 05:21:47 -0700

    Task 0 to Task 8

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - checks digit from 0 through to 9
+ * Return: 0 when successful
+ */
+void print_numbers(void);
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers -  checks for digits from 0 through to 9
+ * Return: 0 when successful
+ */
+void print_most_numbers(void);
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+	}
+	_putchar('\n');
+}
