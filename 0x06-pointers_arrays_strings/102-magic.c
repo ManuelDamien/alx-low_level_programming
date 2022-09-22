6a0d9b4bf104097396caccd26b2ba1cf0314edd6
ManuelDamien <manueldamienboteng@gmail.com>
2022-09-22 08:14:50 -0700

    Task 10 update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - program start point
- * Return: 0 when successful
+ * main - prints a[2] = 98, followed by a new line
+ * Return: 0
  */
 int main(void)
 {
-	int n, a[5], *p;
+	int n;
+	int a[5];
+	int *p;
 
 	a[2] = 1024;
 	p = &n;
