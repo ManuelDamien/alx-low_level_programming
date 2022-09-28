e373499dc114a2334614379458bc6d747e6e16c4
ManuelDamien <manueldamienboteng@gmail.com>
2022-09-28 07:31:14 -0700

    Task 5 update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,28 @@
 #include "main.h"
-/**
- * _evaluate - evaluate fuction sqrt
- * @i: integer
- * @n: integer
- * Return: 0 when successful
- */
-int _evaluate(int i, int n)
-{
-	/*Evaluate function*/
-	if (n == 0 || n == 1)
-	return (n);
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
-	else if (i * i == n) /*condiction base*/
-		return (i);
-	return (-1);
-	return (-1);
-}
-/**
- * _sqrt_recursion - evaluate sqrt
- * @n: integer
- * Return: 0 when successful
- */
-int _sqrt_recursion(int n)
-{
-	int i = 0;
 
-	if (i < 0) /*if n is negative*/
-	{
+/**
+ * _sqrt_recursion - returns the natural square root of the number
+ * @n: number to be used
+ * Return: the square root of n
+ */
+int _sqrt_recursion(int n);
+{
+	if (n == 1 || n == 0)
+		return (n);
+	return (_sqrt(0, n));
+}
+
+/**
+ * _sqrt - square root of an entered number
+ * @n: test number
+ * @x: squared number
+ * Return: the square root of n - 0 when successful
+ */
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
 		return (-1);
-	else
-		return (_evaluate(1, n)); /*Recursive call*/
-	}
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
